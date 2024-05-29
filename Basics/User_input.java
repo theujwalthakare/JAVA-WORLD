5c1cb2ac4c6851e84b854d13e79842148adf87c6
thakare2829 <thakare2829@gmail.com>
2024-05-30 02:37:06 +0530

    input type

---
+++ Basics/User_input.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class User_input {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your string :");
+        // for single string input:->
+        String name = sc.next();
+        System.out.println(name);  
+       
+       // for multiple line input :->
+
+       String Multiline = sc.nextLine();
+       System.out.println(Multiline); 
+
+       // for integer inout:->
+       int number = sc.nextInt();
+       System.out.println(number); 
+       
+       // for floting input :->
+       float num = sc.nextFloat();
+       System.out.println(num);   
+    }
+
+}
