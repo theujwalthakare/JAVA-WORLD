60503e743be6b01c21fe07bcbadd051562823224
Ujwal Thakare <152703428+theujwalthakare@users.noreply.github.com>
2024-05-30 14:25:25 +0530

    Update JavaOut.java

--- Basics/JavaOut.java
+++ Basics/JavaOut.java
@@ -1,7 +1,7 @@
  
 public class JavaOut {
     public static void main(String args[]){
-        System.out.print("Hello !!!"); // print just print and not append on new line 
+        System.out.print("Hello !!!"); // print just print statement and not append on new line 
         System.out.println("Ujwal"); // its append new line
     }
 }
